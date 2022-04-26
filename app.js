b8e60a0508943dd8d88c772d4960e871a7828864
Joel Hoil <yoelvann@gmail.com>
2022-04-25 19:43:30 -0500

    implement GET methods

--- app.js
+++ app.js
@@ -6,6 +6,28 @@ app.use(express.json()) // Indicamos que usaremos JSON
 // Puerto en que vamos a ver nuestra app: localhost:3000
 const port = 3000
 
+// HTPP methods
+app.get('/v1/explorers', (req, res) => {
+    console.log(`Api Explorers GET ALL requests ${ new Date() }`)
+    const explorer1 = { id: 1, name: "Joel1" }
+    const explorer2 = { id: 2, name: "Joel2" }
+    const explorer3 = { id: 3, name: "Joel3" }
+    const explorer4 = { id: 4, name: "Joel4" }
+
+    const explorers = [ explorer1, explorer2, explorer3, explorer4 ]
+
+    res.status(200).json(explorers)
+})
+
+app.get('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers GET request ${ new Date() }`)
+    console.log(`Getting explorer with id ${ req.params.id }`)
+
+    const explorer = { id: 1, name: "Joel" }
+
+    res.status(200).json(explorer)
+})
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
  console.log(`Example app listening on port ${port}`)
